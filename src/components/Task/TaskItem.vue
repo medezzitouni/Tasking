4d8d7f908b57e60925d6dd67664a17bc89d79e70
methe ezzitouni <methe.medpd@gmail.com>
2023-08-03 14:18:43 +0100

    Add TaskList and TaskItem components

---
+++ src/components/Task/TaskItem.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+    import { useStore } from '@/store';
+    const { commit } = useStore();
+    import { SET_SELECTED_TASK } from '@/store/mutation-types'
+    import { toCapital  } from '@/utils';
+    defineProps<{
+    tasks?: any[];
+    }>();
+
+    
+</script>
+
+<template>
+ <div class="h-full">
+    <div v-if="tasks?.length" class="p-3">
+    <template v-for="item in tasks" :key="item.id">
+        <div class="w-full bg-black rounded-xl mb-2 grid place-items-center h-[50px] cursor-pointer shadow-sm hover:shadow-white"
+            @click="commit({
+                type: SET_SELECTED_TASK,
+                selected: item.id
+            })"
+        >
+        {{ toCapital(item.title) }}
+        </div>
+    </template>
+    </div>
+    <div class="text-center" v-else>Empty...</div>
+ </div>
+</template>
---
+++ src/components/Task/TaskList.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+    import { selectTasks } from '../../store/index';
+    import TaskItem from './TaskItem.vue';
+
+</script>
+
+<template>
+ <div class="h-[70vh] w-[35%] flex flex-col pt-1">
+  <TaskItem :tasks="selectTasks()" />
+ </div>
+</template>
+
+<style scoped></style>
