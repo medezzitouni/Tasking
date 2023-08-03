7aeeac1df3eefd197ca9f01266930897a6aac477
methe ezzitouni <methe.medpd@gmail.com>
2023-08-03 14:14:24 +0100

    Set up Project

---
+++ README.md
@@ -0,0 +1,19 @@
+# tasking
+
+## Project Setup
+
+```sh
+npm install
+```
+
+## Run Backend
+
+```sh
+npm run start:api
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
---
+++ vuex.d.ts
@@ -0,0 +1,21 @@
+// import { Store } from 'vuex'
+
+declare module "vuex" {
+    export * from "vuex/types/index.d.ts";
+    export * from "vuex/types/helpers.d.ts";
+    export * from "vuex/types/logger.d.ts";
+    export * from "vuex/types/vue.d.ts";
+}
+
+// declare module '@vue/runtime-core' {
+//   // declare your own store states
+//   interface State {
+//     count: number
+//   }
+
+//   // provide typings for `this.$store`
+//   interface ComponentCustomProperties {
+//     $store: Store<State>
+//   }
+// }
+
