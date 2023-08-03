31c2cc0dac85cc75d97f773808b26fa21e71b62c
methe ezzitouni <methe.medpd@gmail.com>
2023-08-03 14:15:10 +0100

    Set up the api adapter

---
+++ src/utils/api.ts
@@ -0,0 +1,38 @@
+
+import axios, { type AxiosRequestConfig, type AxiosResponse } from "axios";
+
+type AxiosInput = [url: string, config?: AxiosRequestConfig<any> | undefined];
+type AxiosDataInput = [url: string, data?: any, config?: AxiosRequestConfig<any> | undefined];
+
+export default class Api {
+    static AXIOS = axios.create({
+        baseURL: 'http://localhost:3000/'
+    });
+
+    constructor() {}
+
+    static async get(...args: AxiosInput): Promise<AxiosResponse<any, any>> {
+        return Api.AXIOS.get(...args);
+    }
+
+    static async post(...args: AxiosDataInput): Promise<AxiosResponse<any, any>> {
+        return Api.AXIOS.post(...args);
+    }
+
+    static async delete(...args: AxiosInput): Promise<AxiosResponse<any, any>> {
+        return Api.AXIOS.delete(...args);
+    }
+
+    static async put(...args: AxiosDataInput): Promise<AxiosResponse<any, any>> {
+        return Api.AXIOS.put(...args);
+    }
+
+    static async hit({ action, onSuccess, onError }: { action: Promise<AxiosResponse<any, any>> , onSuccess: Function, onError: Function }){
+        try {
+            const result = await action;
+            onSuccess(result.data);
+        } catch (error) {
+            onError(error);
+        }
+    }
+}
